4fd1693fb92a9c2e6d6c3fc66e27b7dfa7c9cea6
Daniel Araujo <daniel.araujo@sedet.ce.gov.br>
2023-04-08 15:37:50 -0300

    oragnizando as rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,6 @@
+import { EditarCidadesComponent } from './cidade/editar-cidades/editar-cidades.component';
+import { InserirCidadesComponent } from './cidade/inserir-cidades/inserir-cidades.component';
+import { ListarCidadesComponent } from './cidade/listar-cidades/listar-cidades.component';
 import { EditarPessoaComponent } from './pessoa/editar-pessoa/editar-pessoa.component';
 import { InserirPessoaComponent } from './pessoa/inserir-pessoa/inserir-pessoa.component';
 import { ListarPessoaComponent } from './pessoa/listar-pessoa/listar-pessoa.component';
@@ -10,6 +13,11 @@ const routes: Routes = [
   { path: 'pessoas/listar', component: ListarPessoaComponent },
   { path: 'pessoas/novo', component: InserirPessoaComponent },
   { path: 'pessoas/editar/:id', component: EditarPessoaComponent },
+
+ // { path: 'cidades', redirectTo: 'cidades/listar' },
+  { path: 'cidades/listar', component: ListarCidadesComponent},
+  { path: 'cidades/novo', component: InserirCidadesComponent },  
+  { path: 'cidades/editar/:id', component: EditarCidadesComponent },
 
 ];
 
