dd0b31d7317ffe9add364e486a10f40d29c1eac0
Daniel Araujo <daniel.araujo@sedet.ce.gov.br>
2023-04-08 15:37:40 -0300

    criando cidade service

--- src/app/cidade/services/cidade.service.ts
+++ src/app/cidade/services/cidade.service.ts
@@ -1,4 +1,7 @@
+import { Cidade } from './../../shared/models/cidade.model';
 import { Injectable } from '@angular/core';
+
+const LS_CHAVE: string = "cidade"
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,38 @@ import { Injectable } from '@angular/core';
 export class CidadeService {
 
   constructor() { }
+
+  listarCidade(): Cidade[]{
+    const cidades = localStorage[LS_CHAVE];
+    return cidades ? JSON.parse(cidades) : []
+  }
+
+  inserir(cidade: Cidade): void {
+    const cidades = this.listarCidade()
+    cidade.id = new Date().getTime()
+    cidades.push(cidade)
+    localStorage[LS_CHAVE] = JSON.stringify(cidades)
+  }
+
+  buscarPorID(id: number): Cidade | undefined{
+    const cidades: Cidade[] = this.listarCidade()
+    return cidades.find(cidade => cidade.id === id)
+  }
+
+  atualizar(cidade: Cidade): void {
+    const cidades: Cidade[] = this.listarCidade()
+
+    cidades.forEach((obj, index, objs) =>{
+      if(cidade.id === obj.id){
+        objs[index] = cidade
+      }
+    })
+    localStorage[LS_CHAVE] = JSON.stringify(cidades)
+  }
+
+  remover(id: number): void{
+    let cidades: Cidade[] = this.listarCidade()
+    cidades = cidades.filter(cidade => cidade.id !== id)
+    localStorage[LS_CHAVE] = JSON.stringify(cidades)
+  }
 }
